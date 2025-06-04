ad3a413ac1eb36d0a29635c4d054d542b5f5d720
Alex Pizarro <alexpizarro3@gmail.com>
2025-06-03 22:21:46 -0600

    Fin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,7 +86,7 @@ export default function Page() {
             alt="Static Hero3D Image"
             className="absolute inset-0 w-full h-full object-cover opacity-90"
           />
-          <div className="absolute inset-0 z-10 flex items-center justify-center px-4 md:px-10 pt-8 md:pt-0">
+          <div className="absolute inset-x-0 bottom-10 z-10 flex items-center justify-center px-4 md:px-10">
             <motion.div
               initial={{ opacity: 0, y: 40 }}
               animate={{ opacity: 1, y: 0 }}
